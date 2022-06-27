4b7f939ecd601c987cbd9eeb2a6ad3731110cd02
Stewart Milne <srm@metabureau.com.au>
2022-06-27 12:35:32 +1000

    install InkLike

--- app.vue
+++ app.vue
@@ -4,3 +4,11 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<style lang="scss">
+:root {
+    --color--primary--h: 195deg;
+    --color--primary--s: 77%;
+    --color--primary--l: 39%;
+}
+</style>
